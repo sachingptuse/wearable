e707acbabd837afd0b4c4d5ef2ce6f8a26799cec
User <sachingptuse@gmail.com>
2025-04-03 12:39:18 +0530

    Initial commit for smartwatch backend

---
+++ src/main/java/com/facilities/wearable/WearableApplication.java
@@ -0,0 +1,13 @@
+package com.facilities.wearable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WearableApplication.class, args);
+	}
+
+}
